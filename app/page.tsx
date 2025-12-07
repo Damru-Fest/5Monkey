c4b05424b80e713112dc10a5df70d5a18e4fd01b
Kevish Sewliya <prokevish07@gmail.com>
2025-12-07 17:53:33 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 import Image from "next/image";
-import { useState, useEffect } from "react";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
 import { motion } from "framer-motion";
 import Hero from "./HomeComponents/Hero";
 import OurStory from "./HomeComponents/OurStory";
@@ -19,27 +16,6 @@ interface Review {
 }
 
 export default function Home() {
-  useEffect(() => {
-      AOS.init({
-        // Global settings:
-        disable: false,
-        startEvent: 'DOMContentLoaded',
-        initClassName: 'aos-init',
-        animatedClassName: 'aos-animate',
-        useClassNames: false,
-        disableMutationObserver: false,
-        debounceDelay: 50,
-        throttleDelay: 99,
-        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-        offset: 120,
-        delay: 0,
-        duration: 1000,
-        easing: 'ease',
-        once: false,
-        mirror: false,
-        anchorPlacement: 'top-bottom',
-      });
-    }, []);
  
 
   return (
